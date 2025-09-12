929c47b465b305864e02a173409fb181382e2210
iNCOHESiONS <incohesions@gmail.com>
2025-09-12 18:20:50 -0300

    ehhh fix

--- src/main/java/io/github/incohesions/immortal_pets/ImmortalPets.java
+++ src/main/java/io/github/incohesions/immortal_pets/ImmortalPets.java
@@ -1,10 +1,13 @@
 package io.github.incohesions.immortal_pets;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.LoggerFactory;
 
-public class Immortal_pets implements ModInitializer {
+public class ImmortalPets implements ModInitializer {
+    public static final String MOD_ID = "immortal_pets";
 
     @Override
     public void onInitialize() {
+        LoggerFactory.getLogger(MOD_ID).info("Mod initialized successfully!");
     }
 }
--- src/main/java/io/github/incohesions/immortal_pets/mixin/LivingEntityMixin.java
+++ src/main/java/io/github/incohesions/immortal_pets/mixin/LivingEntityMixin.java
@@ -1,8 +1,8 @@
 package io.github.incohesions.immortal_pets.mixin;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Tameable;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,16 +10,21 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
-public abstract class TameableEntityMixin {
+public abstract class LivingEntityMixin {
+    @SuppressWarnings("ConstantConditions")
     @Inject(at = @At("HEAD"), method = "damage", cancellable = true)
-    private void onApplyDamage(
+    void onApplyDamage(
         final ServerWorld world,
         final DamageSource source,
         final float amount,
         final CallbackInfoReturnable<Boolean> cir
     ) {
-        if ((Object) this instanceof TameableEntity) {
-            cir.cancel();
-        }
+        Tameable self;
+
+        try {
+            self = (Tameable) this;
+        } catch (ClassCastException ignored) { return; }
+
+        if (self.getOwner() != null) cir.cancel();
     }
 }
